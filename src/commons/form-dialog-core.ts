aaa24956c9de194a89733e3da443737eaa5ded4a
kurukuruz <kurukuruz.4dev@gmail.com>
2020-01-03 16:55:20 +0900

    separate dialog core process

---
+++ src/commons/form-dialog-core.ts
@@ -0,0 +1,80 @@
+import { ref, SetupContext, computed, Ref } from '@vue/composition-api';
+import { useInnerValue, ValueProps } from '@/commons/inner-value';
+import { Division, Entry } from '@/repository';
+import { registerEntry } from '@/repository/dba-entries';
+import { typicalInjection, BookStoreKey, CategoriesStoreKey } from '@/store';
+
+export interface FormDialogProps extends ValueProps<boolean> {
+  date: string;
+}
+
+export function useFormDialog (props: FormDialogProps, context: SetupContext) {
+  const { innerValue } = useInnerValue(props, context);
+
+  const division: Ref<Division> = ref(Division.PAYOUT);
+  const divisionName = computed(() => {
+    switch (division.value) {
+      case Division.PAYOUT: return '支出';
+      case Division.INCOME: return '収入';
+    }
+  });
+  function toggleDivision () {
+    switch (division.value) {
+      case Division.PAYOUT:
+        division.value = Division.INCOME;
+        break;
+      case Division.INCOME:
+        division.value = Division.PAYOUT;
+    }
+  }
+
+  const { allCategoriesList } = typicalInjection(CategoriesStoreKey);
+  const categoriesShown = computed(() => allCategoriesList.value.filter(cate => cate.division === division.value));
+
+  const { bookId } = typicalInjection(BookStoreKey);
+
+  const categoryId = ref('');
+  const memo = ref('');
+  const amount = ref(0);
+  const calcHistory = ref('');
+
+  function reset (): void {
+    categoryId.value = '';
+    memo.value = '';
+    amount.value = 0;
+    calcHistory.value = '';
+  }
+
+  function updateHistory (newValue: string): void {
+    calcHistory.value = newValue;
+  }
+
+  function register (): void {
+    const entry: Entry = {
+      date: props.date,
+      categoryId: categoryId.value,
+      memo: memo.value,
+      division: division.value,
+      amount: amount.value,
+    };
+    registerEntry(bookId.value, entry);
+    closeSelf();
+  }
+
+  function closeSelf (): void {
+    innerValue.value = false;
+  }
+  return {
+    innerValue,
+    categoryId,
+    categoriesShown,
+    divisionName,
+    toggleDivision,
+    memo,
+    amount,
+    calcHistory,
+    updateHistory,
+    register,
+    closeSelf,
+  };
+}
--- src/components/AppFormDialog.vue
+++ src/components/AppFormDialog.vue
@@ -45,18 +45,11 @@
 </template>
 
 <script lang="ts">
-import { createComponent, ref, watch, SetupContext, computed, Ref } from '@vue/composition-api';
-import { useInnerValue, ValueProps } from '@/commons/inner-value';
+import { createComponent, SetupContext } from '@vue/composition-api';
+import { useFormDialog, FormDialogProps } from '@/commons/form-dialog-core';
 import amountFilter from '@/filters/amount-filter';
-import { CategoryDoc, Division, Entry } from '@/repository';
-import { registerEntry } from '@/repository/dba-entries';
-import { typicalInjection, BookStoreKey, CategoriesStoreKey } from '@/store';
 import AppCategorySelect from '@/components/AppCategorySelect.vue';
 import Calculator from '@/components/Calculator.vue';
-
-interface Props extends ValueProps<boolean> {
-  date: string;
-}
 
 export default createComponent({
   components: {
@@ -76,56 +69,21 @@ export default createComponent({
       default: '',
     },
   },
-  setup: (props: Props, context: SetupContext) => {
-    const { innerValue } = useInnerValue(props, context);
+  setup: (props: FormDialogProps, context: SetupContext) => {
+    const {
+      innerValue,
+      categoryId,
+      categoriesShown,
+      divisionName,
+      toggleDivision,
+      memo,
+      amount,
+      calcHistory,
+      updateHistory,
+      register,
+      closeSelf,
+    } = useFormDialog(props, context);
 
-    const division: Ref<Division> = ref(Division.PAYOUT);
-    const divisionName = computed(() => {
-      switch (division.value) {
-        case Division.PAYOUT: return '支出';
-        case Division.INCOME: return '収入';
-      }
-    });
-    function toggleDivision () {
-      switch (division.value) {
-        case Division.PAYOUT:
-          division.value = Division.INCOME;
-          break;
-        case Division.INCOME:
-          division.value = Division.PAYOUT;
-      }
-    }
-
-    const { allCategoriesList } = typicalInjection(CategoriesStoreKey);
-    const categoriesShown = computed(() => allCategoriesList.value.filter(cate => cate.division === division.value));
-
-    const { bookId } = typicalInjection(BookStoreKey);
-
-    const categoryId = ref('');
-
-    const memo = ref('');
-
-    const amount = ref(0);
-    const calcHistory = ref('');
-    function updateHistory (newValue: string): void {
-      calcHistory.value = newValue;
-    }
-
-    function register (): void {
-      const entry: Entry = {
-        date: props.date,
-        categoryId: categoryId.value,
-        memo: memo.value,
-        division: division.value,
-        amount: amount.value,
-      };
-      registerEntry(bookId.value, entry);
-      closeSelf();
-    }
-
-    function closeSelf (): void {
-      innerValue.value = false;
-    }
     return {
       innerValue,
       categoryId,
